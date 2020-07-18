91a4ddead7d4b2c5f3a7445abe5da90d631d187c
Alok Kaintura <66751016+alokkaintura@users.noreply.github.com>
2020-07-18 12:57:07 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<marquee> WECOME TO HYBRID MULTI CLOUD WORLD - BY MR. VIMAL DAGA </marquee> 
+<br />
+<pre>
+<?php
+echo'
+<center> <h2> Terraform - Use Infrastructure as Code to provision and manage any cloud, infrastructure or service </h2> </center>
+';
+?>
+</pre>
